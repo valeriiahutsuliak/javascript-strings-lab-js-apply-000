924b01a919a2271f2560c1eb81d1f14c81ca8805
Valerria Hutsuliak <valeria.gutsuliak@gmail.com>
2019-09-13 21:18:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson"
-
-var greetSpecialGuest == "Hello, + ${specialGuest} + !"
-
-var space = "Let\'s talk about space"
-
-var conversation = `${space}`;
+ 
+var greetSpecialGuest ="" + specialGuest + "!";
+ 
+var topic = "space";
+ 
+var conversation = `${topic}`;
